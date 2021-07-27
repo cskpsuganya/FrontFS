8dd13559db8b441d7f1b52baa85ce5b4bb05effb
adithya-adi-menon <adithyamenons17@gmail.com>
2021-07-27 23:16:13 +0530

    added pojo

---
+++ src/main/java/com/example/backend/Model/ProductModel.java
@@ -0,0 +1,66 @@
+package com.example.backend.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ProductModel {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private String productId;
+    private String imageUrl;
+    private String productName;
+    private String price;
+    private String description;
+    private String quantity;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
