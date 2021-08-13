d81c74c0099a028d0806841ee93c041c04f6e2ed
npc203 <npc203@users.noreply.github.com>
2021-08-13 18:14:20 +0530

    oops missed this

--- src/components/User/home.js
+++ src/components/User/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 import { Card} from 'material-ui';
@@ -7,8 +7,9 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import { Box } from '@material-ui/core';
+import PropTypes from 'prop-types';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
   card: {
     maxWidth: 240,
     borderColor: "black",
@@ -30,22 +31,20 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor:"grey",
     height: 27,
   }
-}));
+});
 
 function MediaCard(props) {
-  const classes = useStyles();
-
   return (
     <Grid item xs={2}>
     <Box border={1}>
-    <Card className={classes.card}>
+    <Card className={props.classes.card}>
       <CardActionArea>
         <CardMedia
-          className={classes.media}
+          className={props.classes.media}
           image={props.src}
           title="Name"
         />
-        <CardContent className={classes.content}>
+        <CardContent className={props.classes.content}>
           <Typography>
             {props.name}  ₹{props.price}
           </Typography>
@@ -57,9 +56,10 @@ function MediaCard(props) {
   );
 }
 
-export default function HomeBody() {
-  const classes = useStyles();
-
+class HomeBody extends React.Component {
+  
+  render(){
+  const classes = this.props;
   return (
     <div className={classes.root}>
       <div className={classes.banner}>
@@ -72,10 +72,18 @@ export default function HomeBody() {
         <Grid container spacing={3} direction="row">
         {/* TODO backend card gen here */}
         {Array(20).fill(1).map((el, i) =>
-          <MediaCard price="100" name="ProductName" src="https://m.media-amazon.com/images/I/41eBIiMMjbL._SY355_.jpg"/>
+          <MediaCard classes={classes} price="100" name="ProductName" src="https://m.media-amazon.com/images/I/41eBIiMMjbL._SY355_.jpg"/>
         )}
         </Grid>
       </div>
     </div>
   );
 }
+}
+
+HomeBody.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(useStyles)(HomeBody);
